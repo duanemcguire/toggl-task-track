725a57d5e39e1acf54997686cefea451cb8b9e4c
Duane McGuire <biz@duanemcguire.com>
2022-10-14 07:26:14 -0600

    Clean up https requests using the requests module

--- togglTaskStartStop.py
+++ togglTaskStartStop.py
@@ -1,21 +1,21 @@
 #togglTaskStartStop.py
-#
+
 # Query the status of the Shop Lights
 # If they have changed on/off status since the last check
 # Either (if lights came on) start a generic "Lights On" time track in Toggl/Track
 # Or (if lights went off) stop the current time tracker in Toggl/Track
 
 
-import urllib.request, urllib.error, urllib.parse
 import xml.etree.ElementTree as ET
 import json
 import os
 import requests
-from base64 import b64encode
 from datetime import datetime
 
-
-toggl_auth = os.getenv("TOGGL_AUTH").encode()  # TOGGL_AUTH is <email>:<password>
+isy_user_name = os.getenv("ISY_USER")
+isy_user_pass = os.getenv("ISY_PASS")
+toggl_user = os.getenv("TOGGL_USER")
+toggl_pass = os.getenv("TOGGL_PASS")
 toggl_workspace = int(os.getenv("TOGGL_WORKSPACE")) #  integer workspace id at toggl.com
 toggl_shop_general = int(os.getenv("TOGGL_SHOP_GENERAL")) # integer project_id for my "shop general" project
 shop_lights_status_file = os.getenv("SHOP_LIGHTS_STATUS_FILE") # string file location of status file
@@ -26,35 +26,20 @@ shop_lights_on = False
 prior_shop_lights_on = False
 # Get prior status - shop_lights_status_file will have one of two words:  on|off
 with open(shop_lights_status_file) as f:
-    prior_shop_lights_on = f.read() == "on"
+    prior_shop_lights_on = f.read() == "True"
 
 # local address for the ISY device is kept in /etc/hosts
-ISYaddr = "http://isy"
+isy_addr = "http://isy/rest/nodes"
 
 
 # Get current info from isy944i
-# create a password manager
-password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
 
-# Add the username and password.
-top_level_url = ISYaddr
-user_name = os.getenv("ISY_USER")
-user_pass = os.environ.get("ISY_PASS")
-password_mgr.add_password(None, top_level_url, user_name, user_pass)
-handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
-
-# create "opener" (OpenerDirector instance)
-opener = urllib.request.build_opener(handler)
-
-# use the opener to fetch a URL
-opener.open(ISYaddr)
-
-# Install the opener.
-urllib.request.install_opener(opener)
-response = urllib.request.urlopen(ISYaddr + "/rest/nodes")
-data = response.read()
-root = ET.fromstring(data)
-onCount = 0
+response = requests.get(
+    isy_addr,
+    auth=(isy_user_name,isy_user_pass)
+)
+root = ET.fromstring(response.content)
+on_count = 0
 for node in root:
     device = node.attrib.get("nodeDefId")
     if (
@@ -66,11 +51,11 @@ for node in root:
             name = node.findall("name")[0].text
             status = node.findall("property")[0].attrib.get("value")
             if int(status) > 0 and name == "Shop 1":
-                onCount = onCount + 1
+                on_count = on_count + 1
             if int(status) > 0 and name == "Shop 2":
-                onCount = onCount + 1
-            if onCount > 1:
-                shop_lights_on = "on"
+                on_count = on_count + 1
+            if on_count > 1:
+                shop_lights_on = True
         except Exception as e:
             print(str(e))
             print(property[0].attrib)
@@ -81,8 +66,7 @@ print("prior", prior_shop_lights_on)
 
 # Store shop lights status for toggle comparison next time
 with open(shop_lights_status_file, "w") as f:
-    f.write("on") if shop_lights_on else f.write("off")
-
+    f.write(str(shop_lights_on))
 
 # Finally, start or stop a Toggl time entry if the shop lights status has changed. 
 
@@ -107,37 +91,38 @@ if shop_lights_on and not prior_shop_lights_on:
         "stop": None,
     }
 
-
+    url = "/".join((
+        "https://api.track.toggl.com/api/v9/workspaces",
+        str(toggl_workspace),
+        "time_entries"
+    ))
+    
     data = requests.post(
-        "https://api.track.toggl.com/api/v9/workspaces/" + str(toggl_workspace) + "/time_entries",
-        json=j,
-        headers={
-            "content-type": "application/json",
-            "Authorization": "Basic %s" % b64encode(toggl_auth).decode("ascii"),
-        },
-    )
-    print(data.json())
+        url, 
+        json=j, 
+        auth=(toggl_user,toggl_pass) 
+        )
+
 if  not shop_lights_on and prior_shop_lights_on:
     # Get current Time entry
     data = requests.get(
         "https://api.track.toggl.com/api/v9/me/time_entries/current",
-        headers={
-            "content-type": "application/json",
-            "Authorization": "Basic %s" % b64encode(toggl_auth).decode("ascii"),
-        },
+         auth=(toggl_user,toggl_pass) 
     )
     jdata = data.json()
-    workspace = jdata["workspace_id"]
-    time_id = jdata["id"]
-    # Stop current Time entry
-    data = requests.patch(
-        "https://api.track.toggl.com/api/v9/workspaces/"
-        + str(workspace)
-        + "/time_entries/"
-        + str(time_id)
-        + "/stop",
-        headers={
-            "content-type": "application/json",
-            "Authorization": "Basic %s" % b64encode(toggl_auth).decode("ascii"),
-        },
-    )
+    if len(jdata) :
+        workspace = jdata["workspace_id"]
+        time_id = jdata["id"]
+        url = "/".join((
+            "https://api.track.toggl.com/api/v9/workspaces",
+            str(toggl_workspace),
+            "time_entries",
+            str(time_id),
+            "stop"
+        ))  
+        # Stop current Time entry
+        data = requests.patch(
+            url,
+            auth=(toggl_user,toggl_pass)
+        )
+        
